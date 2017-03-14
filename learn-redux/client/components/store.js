a3483ed19da786642e911ea2df0b27e4a321b235
Taj English <tajenglish@gmail.com>
2017-03-14 19:45:55 -0400

    Creating Redux Store

---
+++ learn-redux/client/components/store.js
@@ -0,0 +1,16 @@
+import { createStore, compose } from 'redux';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { browserHistory } from 'react-router';
+
+//import root reducer
+import rootReducer from './reducers/index';
+
+import comments from './data/comments';
+import posts from './data/posts';
+
+//create an object for the defualt data
+
+const defaultState = {
+  posts,
+  comments
+}
