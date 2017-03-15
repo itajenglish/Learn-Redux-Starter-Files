60043188c1d6e8e825ecae1b22c9c1aed365cf81
Taj English <tajenglish@gmail.com>
2017-03-14 21:24:06 -0400

    Integrating our state with react-router

--- learn-redux/client/store.js
+++ learn-redux/client/store.js
@@ -9,8 +9,14 @@ import comments from './data/comments';
 import posts from './data/posts';
 
 //create an object for the defualt data
-
 const defaultState = {
   posts,
   comments
 }
+
+const store = createStore(rootReducer, defaultState)
+
+
+export const history = syncHistoryWithStore(browserHistory, store);
+
+export default store;
