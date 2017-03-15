0cb7ee6fe2288fe9d46b19b1d51a6fbfa6effaa8
Taj English <tajenglish@gmail.com>
2017-03-14 20:10:16 -0400

    Creating Action Creators

---
+++ learn-redux/client/actions/actionCreator.js
@@ -0,0 +1,24 @@
+//increment
+export function increment(index) {
+  return {
+    type: 'INCREMENT_LIKES',
+    index
+  }
+}
+
+//add comment
+export function addComment(postId, author, comment) {
+  return {
+    type: 'ADD_COMMENT',
+    postId,
+    author,
+    comment
+  }
+}
+//remove comment
+export function removeComent(postId, index) {
+  return {
+    type: 'REMOVE_COMMENT',
+    index
+  }
+}
